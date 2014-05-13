e4467244c8f4f9edf2b290ee43382a8a052b7d0b
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-05-13 15:20:02 +0200

    Fixed simulated xtion rate reducer launcher

---
+++ scripts/set_simulated_xtion_dyn_params.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on May 13 2014
+
+@author: Sam Pfeiffer
+
+Set dynamic params for the simulated Xtion of REEM.
+We must lower the quantity of data sent.
+
+
+"""
+
+import rospy
+from dynamic_reconfigure import client
+
+
+if __name__=='__main__':
+    rospy.init_node("set_simulated_xtion_dyn_params", anonymous=True)
+    rospy.loginfo("Trying to connect a service client to head_mount_xtion dynamic reconfigure...")
+    client = client.Client("/head_mount_xtion")
+    rospy.loginfo("Got a client! Setting parameters.")
+    params = { 'imager_rate:' : 1.0 } # drop rate
+    config = client.update_configuration(params)
+    # check if it was really set
+    
+    rospy.loginfo("Parameters set: " + str(config))
+
